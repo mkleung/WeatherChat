f79254e60bf8af8dccae5c444b06d68c952d0f5e
mike <mpakleung@gmail.com>
2020-02-11 17:16:54 -0500

    Create the ProfileActivity

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -17,53 +17,9 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     SharedPreferences prefs = null;
-
-
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        //setContentView(R.layout.activity_main);
-//
-//        //setContentView(R.layout.activity_main_linear);
-//        //setContentView(R.layout.activity_main_grid);
-//        setContentView(R.layout.activity_main_relative);
-//
-//        // Step 4
-//        Button clickButton = findViewById(R.id.button2);
-//        clickButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v){
-//                String toast_message = getString(R.string.toast_message);
-//                Toast.makeText(MainActivity.this, toast_message, Toast.LENGTH_LONG).show();
-//            }
-//        });
-//
-//        // Step 5
-//        String on_message = getString(R.string.on_message);
-//        String off_message = getString(R.string.off_message);
-//        String undo_message = getString(R.string.undo_message);
-//
-//        CheckBox cb = findViewById(R.id.checkBox);
-//        cb.setOnCheckedChangeListener( (compoundButton, b) -> {
-//            String checkbox_text = getString(R.string.checkbox_text);
-//            if ( b == true) { checkbox_text = checkbox_text + " " + on_message; }
-//            else { checkbox_text = checkbox_text + " " + off_message; }
-//            Snackbar.make(cb, checkbox_text, Snackbar.LENGTH_LONG)
-//                    .setAction(undo_message, click-> cb.setChecked( !b ))
-//                    .show();
-//        });
-//
-//        Switch sw = findViewById(R.id.switch1);
-//        sw.setOnCheckedChangeListener( (compoundButton, b) -> {
-//            String switch_text = getString(R.string.switch_text);
-//            if ( b == true) { switch_text = switch_text + " " + on_message; }
-//            else { switch_text = switch_text + " " + off_message; }
-//            Snackbar.make(sw, switch_text, Snackbar.LENGTH_LONG)
-//                    .setAction(undo_message, click-> cb.setChecked( !b ))
-//                    .show();
-//        });
-//    }
 
 
     @Override
@@ -71,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main_login);
+
 
     }
 
@@ -83,11 +40,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
 
+
         // Week3, Page20 - The String fileName specifies the name of the file, mode is the security permissions
         prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
         String savedString = prefs.getString("ReserveName", "Enter something");
-        EditText typeField = findViewById(R.id.emailText);
-        typeField.setText(savedString);
+        EditText emailText = findViewById(R.id.emailText);
+        emailText.setText(savedString);
+
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener( bt -> saveSharedPrefs( emailText.getText().toString()) );
+
     }
 
     @Override
