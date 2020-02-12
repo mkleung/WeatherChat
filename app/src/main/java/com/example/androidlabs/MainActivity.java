23a152e85b51bedd953530acc30ae3f9848d11cd
mike <mpakleung@gmail.com>
2020-02-11 19:47:05 -0500

    Add shared services and intent

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -17,10 +18,9 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-    SharedPreferences prefs = null;
-
+    EditText emailText;
+    Button loginButton;
+    Button profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,41 +28,71 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main_login);
 
+        emailText = (EditText)findViewById(R.id.emailText);
+
+
+        // Go to profile activity
+        profileButton = (Button) findViewById(R.id.profileButton);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Save email
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences=getSharedPreferences("MyData", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putString("email",  emailText.getText().toString());
+                editor.commit();
+
+                Toast.makeText(getApplicationContext(), "Data was saved successfully ", Toast.LENGTH_LONG).show();
+
+            }
+        });
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//
+//        // Week3, Page20 - The String fileName specifies the name of the file, mode is the security permissions
+//        prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+//        String savedString = prefs.getString("ReserveName", "Enter something");
+//        EditText emailText = findViewById(R.id.emailText);
+//        emailText.setText(savedString);
+//        Button loginButton = findViewById(R.id.loginButton);
+//        loginButton.setOnClickListener( bt -> saveSharedPrefs( emailText.getText().toString()) );
+//
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//    }
+//
+//
+//    private void saveSharedPrefs(String stringToSave)
+//    {
+//        SharedPreferences.Editor editor = prefs.edit();
+//        editor.putString("ReserveName", stringToSave);
+//        editor.commit();
+//    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
 
 
-        // Week3, Page20 - The String fileName specifies the name of the file, mode is the security permissions
-        prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
-        String savedString = prefs.getString("ReserveName", "Enter something");
-        EditText emailText = findViewById(R.id.emailText);
-        emailText.setText(savedString);
 
-        Button loginButton = findViewById(R.id.loginButton);
-
-        loginButton.setOnClickListener( bt -> saveSharedPrefs( emailText.getText().toString()) );
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-
-    private void saveSharedPrefs(String stringToSave)
-    {
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("ReserveName", stringToSave);
-        editor.commit();
-    }
 }
--- app/src/main/java/com/example/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -2,13 +2,124 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+
+    EditText emailText;
+    Button loadButton;
+    Button previousButton;
+
+    public static final String DEFAULT="N/A";
+
+    ImageButton mImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        emailText = (EditText)findViewById(R.id.emailText);
+
+
+        // Load Button
+        loadButton = (Button) findViewById(R.id.loadButton);
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences sharedPreferences = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+                String email = sharedPreferences.getString("email",DEFAULT);
+
+                if (email.equals(DEFAULT)) {
+                    Toast.makeText(getApplicationContext(), "No data found", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Data loaded successfully", Toast.LENGTH_LONG).show();
+                    emailText.setText(email);
+                }
+            }
+        });
+
+        // Previous Button
+        previousButton = (Button) findViewById(R.id.previousButton);
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        Log.e(ACTIVITY_NAME, "In function: onCreate");
     }
+
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageMananger()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            mImageButton.setImageBitmap(imageBitmap);
+//        }
+//    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "In function: onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "In function: onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "In function: onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "In function: onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "In function: onDestroy");
+    }
+
+
+
 }
