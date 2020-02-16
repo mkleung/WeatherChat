a59788120419c43ccacf2090d02405bd1045cf3d
mike <mpakleung@gmail.com>
2020-02-16 14:44:49 -0500

    Add ListView in chatroom

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -1,14 +1,63 @@
 package com.example.androidlabs;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ChatRoomActivity extends AppCompatActivity {
+
+    private ArrayList<String> elements = new ArrayList<>( Arrays.asList( "One", "Two" ) );
+    private MyListAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
+
+
+
+        ListView myList = findViewById(R.id.theListView);
+        myList.setAdapter( myAdapter = new MyListAdapter());
+        myList.setOnItemClickListener( (parent, view, pos, id) -> {
+                elements.remove(pos);
+                myAdapter.notifyDataSetChanged();
+            }
+        );
+
+
     }
+
+    private class MyListAdapter extends BaseAdapter{
+        public int getCount() { return elements.size();}
+        public Object getItem(int position) { return "This is row " + position; }
+        public long getItemId(int position) { return (long) position; }
+        public View getView(int position, View old, ViewGroup parent)
+        {
+            LayoutInflater inflater = getLayoutInflater();
+
+            //make a new row:
+            View newView = inflater.inflate(R.layout.row_layout, parent, false);
+
+            //set what the text should be for this row:
+            TextView tView = newView.findViewById(R.id.textGoesHere);
+            tView.setText( getItem(position).toString() );
+
+            //return it to be put in the table
+            return newView;
+        }
+    }
+
+
 }
