c1e4095321338105bceb74003ccf387e3cfa7ce0
mike <mpakleung@gmail.com>
2020-02-23 15:13:04 -0500

    Lab 5 - Major refactoring of Message class to Chat class

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -25,13 +25,9 @@ import java.util.ArrayList;
 
 public class ChatRoomActivity extends AppCompatActivity {
 
-    ArrayList<Message> messageList = new ArrayList<Message>();
-
-    private EditText messageText;
-    private Button sendButton;
-    private Button receivedButton;
-    private String messageContent;
-
+    ArrayList<Chat> chatList = new ArrayList<>();
+    private static int ACTIVITY_VIEW_CONTACT = 33;
+    int positionClicked = 0;
     MyOwnAdapter myAdapter;
     SQLiteDatabase db;
 
@@ -40,221 +36,175 @@ public class ChatRoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
 
-        // Database
-        loadDataFromDatabase();
+        //Get the fields from the screen:
+        EditText chatEdit = (EditText)findViewById(R.id.chatEdit);
 
+        Button insertButton = (Button)findViewById(R.id.sendButton);
+        ListView theList = (ListView)findViewById(R.id.the_list);
 
-        ListView myList = findViewById(R.id.theListView);
+        loadDataFromDatabase(); //get any previously saved Contact objects
+
+        //create an adapter object and send it to the listVIew
         myAdapter = new MyOwnAdapter();
-        myList.setAdapter(myAdapter);
-//
-//
-//        myList.setAdapter(myAdapter = new MessagesAdapter());
-//        myList.setOnItemLongClickListener((p, b, pos, id) -> {
-//            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-//
-//
-//            alertDialogBuilder.setTitle(getString(R.string.alert_title))
-//                    .setMessage(getString(R.string.alert_description_1) + ": " + pos + "\n" + getString(R.string.alert_description_2) + ": " + pos)
-//                    .setPositiveButton(R.string.alert_yes, (click, arg) -> {
-//                        messageList.remove(pos);
-//                        myAdapter.notifyDataSetChanged();
-//                    })
-//                    .setNegativeButton(R.string.alert_no, (click, arg) -> {
-//                    })
-//                    .setView(getLayoutInflater().inflate(R.layout.row_layout_receive, null))
-//                    .create().show();
-//            return true;
-//        });
+        theList.setAdapter(myAdapter);
 
 
-        Message msg1 = new Message(1, "Hello", "sender");
-        Message msg2 = new Message(2, "World", "receiver");
-        messageList.add(msg1);
-        messageList.add(msg2);
-
-
-        messageText = (EditText) findViewById(R.id.messageText);
-        sendButton = (Button) findViewById(R.id.sendButton);
+        //This listens for items being clicked in the list view
+        theList.setOnItemClickListener(( parent,  view,  position,  id) -> {
+           // showContact( position );
+        });
 
 
         //Listen for an insert button click event:
-        sendButton.setOnClickListener(click ->
+        insertButton.setOnClickListener( click ->
         {
+            //get the email and name that were typed
+            String name = chatEdit.getText().toString();
 
-            String title = messageText.getText().toString();
-
-
+            //add to the database and get the new ID
             ContentValues newRowValues = new ContentValues();
 
-            newRowValues.put(DbOpener.COL_TITLE, title);
-            newRowValues.put(DbOpener.COL_ORIGIN, "sender");
+            //Now provide a value for every database column defined in MyOpener.java:
+            //put string name in the NAME column:
+            newRowValues.put(DbOpener.COL_TITLE, name);
 
+            //Now insert in the database:
             long newId = db.insert(DbOpener.TABLE_NAME, null, newRowValues);
 
-            Message newMessage = new Message(newId, title, "sender");
+            //now you have the newId, you can create the Contact object
+            Chat newContact = new Chat(name, "sender", newId);
 
             //add the new contact to the list:
-            messageList.add(newMessage);
+            chatList.add(newContact);
             //update the listView:
+            myAdapter.notifyDataSetChanged();
 
+            //clear the EditText fields:
+            chatEdit.setText("");
 
             //Show the id of the inserted item:
-            Toast.makeText(this, "Inserted item id:" + newId, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Inserted item id:"+newId, Toast.LENGTH_LONG).show();
         });
     }
 
-    protected class MyOwnAdapter extends BaseAdapter {
-        @Override
-        public int getCount() {
-            return messageList.size();
-        }
 
-        public Message getItem(int position){
-            return messageList.get(position);
-        }
-
-        public long getItemId(int position)
-        {
-            return getItem(position).getId();
-        }
-
-        public View getView(int position, View old, ViewGroup parent)
-        {
-            View newView = getLayoutInflater().inflate(R.layout.row_layout_send, parent, false );
-
-            Message thisRow = getItem(position);
-
-            //get the TextViews
-            TextView rowTitle = (TextView)newView.findViewById(R.id.messageTitle);
-            TextView rowOrigin = (TextView)newView.findViewById(R.id.messageOrigin);
-            TextView rowId = (TextView)newView.findViewById(R.id.messageId);
-
-            //update the text fields:
-            rowTitle.setText(  thisRow.getTitle());
-            rowOrigin.setText( thisRow.getOrigin());
-            rowId.setText("id:" + thisRow.getId());
-
-            //return the row:
-            return newView;
-        }
-    }
-
-
-//        messageText = (EditText) findViewById(R.id.messageText);
-//        sendButton = (Button) findViewById(R.id.sendButton);
-//        sendButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (!TextUtils.isEmpty(messageText.getText().toString())) {
-//                    messageList.add(new Message(3, messageText.getText().toString(), true));
-//                    messageText.setText("");
-//                    myAdapter.notifyDataSetChanged();
-//                    closeKeyboard();
-//                }
-//            }
-//        });
-//
-//        receivedButton = (Button) findViewById(R.id.receivedButton);
-//        receivedButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (!TextUtils.isEmpty(messageText.getText().toString())) {
-//                    messageList.add(new Message(4, messageText.getText().toString(), false));
-//                    messageText.setText("");
-//                    myAdapter.notifyDataSetChanged();
-//                    closeKeyboard();
-//                }
-//            }
-//        });
-
-        // ListView
-
-
-
-
-        //Whenever you swipe down on the list, do something:
-//        SwipeRefreshLayout refresher = findViewById(R.id.refresher);
-//        refresher.setOnRefreshListener( () -> refresher.setRefreshing(false)  );
-
-//
-//    protected class MessagesAdapter extends BaseAdapter {
-//        public int getCount() {
-//            return messageList.size();
-//        }
-//
-//        public Object getItem(int position) {
-////            return "This is row " + position;
-//            return messageList.get(position);
-//        }
-//
-//
-//        public long getItemId(int position) {
-//            return (long) position;
-//
-//            //return getItem(position).getId();
-//        }
-//
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//            Message message = messageList.get(position);
-//            int imageResource;
-//            int row_layout;
-//            if (message.origin.equals("sender")) {
-//                row_layout = R.layout.row_layout_send;
-//                imageResource = R.drawable.row_send;
-//            } else {
-//                row_layout = R.layout.row_layout_receive;
-//                imageResource = R.drawable.row_receive;
-//            }
-//            View newView = getLayoutInflater().inflate(row_layout, parent, false);
-//            TextView messageView = (TextView) newView.findViewById(R.id.messageView);
-//            messageView.setText(message.title);
-//            ImageView imageView = (ImageView) newView.findViewById(R.id.imageView);
-//            imageView.setImageResource(imageResource);
-//            return newView;
-//        }
-//    }
-
-    // Close The Virtual keyboard
-    private void closeKeyboard() {
-        // current edittext
-        View view = this.getCurrentFocus();
-        // if there is a view that has focus
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
-    }
-
-
-    public void loadDataFromDatabase() {
-
+    private void loadDataFromDatabase()
+    {
         //get a database connection:
         DbOpener dbOpener = new DbOpener(this);
         db = dbOpener.getWritableDatabase();
 
-
         // We want to get all of the columns. Look at MyOpener.java for the definitions:
-        String [] columns = {DbOpener.COL_ID, DbOpener.COL_TITLE, DbOpener.COL_ORIGIN};
+        String [] columns = {DbOpener.COL_ID, DbOpener.COL_TITLE, DbOpener.COL_TYPE};
         //query all the results from the database:
         Cursor results = db.query(false, DbOpener.TABLE_NAME, columns, null, null, null, null, null, null);
 
         //Now the results object has rows of results that match the query.
         //find the column indices:
         int titleColumnIndex = results.getColumnIndex(DbOpener.COL_TITLE);
-        int originColIndex = results.getColumnIndex(DbOpener.COL_ORIGIN);
+        int typeColumnIndex = results.getColumnIndex(DbOpener.COL_TYPE);
         int idColIndex = results.getColumnIndex(DbOpener.COL_ID);
 
         //iterate over the results, return true if there is a next item:
         while(results.moveToNext())
         {
             String title = results.getString(titleColumnIndex);
-            String origin = results.getString(originColIndex);
+            String type = results.getString(typeColumnIndex);
             long id = results.getLong(idColIndex);
 
             //add the new Contact to the array list:
-            messageList.add(new Message(id, "Hello", "sender"));
+            chatList.add(new Chat(title, type, id));
         }
+
+        //At this point, the contactsList array has loaded every row from the cursor.
     }
 
+
+//    protected void showContact(int position)
+//    {
+//        Chat selectedMessage = chatList.get(position);
+//
+//        View contact_view = getLayoutInflater().inflate(R.layout.row_layout_send, null);
+//        //get the TextViews
+//        EditText rowName = contact_view.findViewById(R.id.row_name);
+//        EditText rowEmail = contact_view.findViewById(R.id.row_email);
+//        TextView rowId = contact_view.findViewById(R.id.row_id);
+//
+//        //set the fields for the alert dialog
+//        rowName.setText(selectedContact.getName());
+//        rowEmail.setText(selectedContact.getEmail());
+//        rowId.setText("id:" + selectedContact.getId());
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("You clicked on item #" + position)
+//                .setMessage("You can update the fields and then click update to save in the database")
+//                .setView(contact_view) //add the 3 edit texts showing the contact information
+//                .setPositiveButton("Update", (click, b) -> {
+//                    selectedContact.update(rowName.getText().toString(), rowEmail.getText().toString());
+//                    updateContact(selectedContact);
+//                    myAdapter.notifyDataSetChanged(); //the email and name have changed so rebuild the list
+//                })
+//                .setNegativeButton("Delete", (click, b) -> {
+//                    deleteContact(selectedContact); //remove the contact from database
+//                    chatList.remove(position); //remove the contact from contact list
+//                    myAdapter.notifyDataSetChanged(); //there is one less item so update the list
+//                })
+//                .setNeutralButton("dismiss", (click, b) -> { })
+//                .create().show();
+//    }
+
+    protected void updateContact(Chat c)
+    {
+        //Create a ContentValues object to represent a database row:
+        ContentValues updatedValues = new ContentValues();
+        updatedValues.put(DbOpener.COL_TITLE, c.getTitle());
+        updatedValues.put(DbOpener.COL_TYPE, c.getType());
+
+        //now call the update function:
+        db.update(DbOpener.TABLE_NAME, updatedValues, DbOpener.COL_ID + "= ?", new String[] {Long.toString(c.getId())});
+    }
+
+    protected void deleteContact(Chat c)
+    {
+        db.delete(DbOpener.TABLE_NAME, DbOpener.COL_ID + "= ?", new String[] {Long.toString(c.getId())});
+    }
+
+    //This class needs 4 functions to work properly:
+    protected class MyOwnAdapter extends BaseAdapter
+    {
+        @Override
+        public int getCount() {
+            return chatList.size();
+        }
+
+        public Chat getItem(int position){
+            return chatList.get(position);
+        }
+
+        public View getView(int position, View old, ViewGroup parent)
+        {
+            View newView = getLayoutInflater().inflate(R.layout.row_layout_send, parent, false );
+
+            Chat thisRow = getItem(position);
+
+            //get the TextViews
+            TextView rowTitle = (TextView)newView.findViewById(R.id.chatTitle);
+            TextView rowType = (TextView)newView.findViewById(R.id.chatType);
+            TextView rowId = (TextView)newView.findViewById(R.id.chatId);
+
+            //update the text fields:
+            rowTitle.setText(  thisRow.getTitle());
+            rowType.setText( thisRow.getType());
+            rowId.setText("id:" + thisRow.getId());
+
+            //return the row:
+            return newView;
+        }
+
+        //last week we returned (long) position. Now we return the object's database id that we get from line 73
+        public long getItemId(int position)
+        {
+            return getItem(position).getId();
+        }
+    }
 }
