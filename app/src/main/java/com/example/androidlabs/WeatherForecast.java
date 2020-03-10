06b6e1a546fa870792df78013e066953ce07c247
mike <mpakleung@gmail.com>
2020-03-10 02:44:23 -0400

    Lab 6 - Add UV api callback

--- app/src/main/java/com/example/androidlabs/WeatherForecast.java
+++ app/src/main/java/com/example/androidlabs/WeatherForecast.java
@@ -11,13 +11,17 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import org.json.JSONObject;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WeatherForecast extends AppCompatActivity {
 
@@ -30,8 +34,11 @@ public class WeatherForecast extends AppCompatActivity {
     TextView currentTempText;
     TextView minTempText;
     TextView maxTempText;
+    TextView uvRatingText;
 
     private static final String TAG = "WeatherForecast";
+    private static final String urlTemp = "http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=7e943c97096a9784391a981c4d878b22&mode=xml&units=metric";
+private static final String urlUV = "http://api.openweathermap.org/data/2.5/uvi?appid=7e943c97096a9784391a981c4d878b22&lat=45.348945&lon=-75.759389";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,21 +51,13 @@ public class WeatherForecast extends AppCompatActivity {
         currentTempText = (TextView) findViewById(R.id.currentTempText);
         minTempText = (TextView) findViewById(R.id.minTempText);
         maxTempText = (TextView) findViewById(R.id.maxTempText);
-
+        uvRatingText = (TextView) findViewById(R.id.uvRatingText);
 
         ForecastQuery req = new ForecastQuery();
-        req.execute("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=7e943c97096a9784391a981c4d878b22&mode=xml&units=metric");
+        req.execute(urlTemp);
 
-
-
-
-//        ForecastQuery uvreq = new ForecastQuery();
-//        uvreq.execute("http://api.openweathermap.org/data/2.5/uvi?appid=7e943c97096a9784391a981c4d878b22&lat=45.348945&lon=-75.759389");
-
-
-    }
-
-    public void startAsyncTask(View v) {
+//        ForecastQuery req2 = new ForecastQuery();
+//        req2.execute(urlUV);
 
     }
 
@@ -70,28 +69,24 @@ public class WeatherForecast extends AppCompatActivity {
         public String uv;
         public Bitmap bm;
 
-
         private static final String TAG = "ForecastQuery";
 
         protected void onPreExecute() {
             currentTempText.setVisibility(View.GONE);
             minTempText.setVisibility(View.GONE);
             maxTempText.setVisibility(View.GONE);
+            uvRatingText.setVisibility(View.GONE);
         }
 
-        protected String doInBackground(String ... args)
-        {
+        protected String doInBackground(String ... args) {
 
             try {
 
-                URL url = new URL(args[0]);
-
-                //open the connection
+                URL url = new URL(urlTemp);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-                //wait for data:
                 InputStream response = urlConnection.getInputStream();
 
+                // TEMPERATURE
                 XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
                 factory.setNamespaceAware(false);
                 XmlPullParser xpp = factory.newPullParser();
@@ -103,9 +98,21 @@ public class WeatherForecast extends AppCompatActivity {
                 while(eventType != XmlPullParser.END_DOCUMENT) {
                     if(eventType == XmlPullParser.START_TAG) {
                         if(xpp.getName().equals("temperature")) {
+
+                            Thread.sleep(200);
                             currentTemp = xpp.getAttributeValue(null,    "value");
+                            publishProgress(25);
+
+                            Thread.sleep(200);
                             minTemp = xpp.getAttributeValue(null,    "min");
+                            publishProgress(50);
+
+                            Thread.sleep(200);
                             maxTemp = xpp.getAttributeValue(null, "max");
+                            publishProgress(75);
+
+
+
                         }
                         else if(xpp.getName().equals("lastupdate"))
                         {
@@ -117,23 +124,30 @@ public class WeatherForecast extends AppCompatActivity {
                 }
 
 
-                //JSON reading:
-                //Build the entire string response:
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
-//            StringBuilder sb = new StringBuilder();
-//
-//            String line = null;
-//            while ((line = reader.readLine()) != null)
-//            {
-//                sb.append(line + "\n");
-//            }
-//            String result = sb.toString(); //result is the whole string
-//
+                // UV
+                URL url2 = new URL(urlUV);
+                HttpURLConnection urlConnection2 = (HttpURLConnection) url2.openConnection();
+                InputStream response2 = urlConnection2.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response2, "UTF-8"), 8);
+                StringBuilder sb = new StringBuilder();
 
-//            JSONObject uvReport = new JSONObject(result);
-//
-//            double uvRating = uvReport.getDouble("value");
-//            Log.i("MainActivity", "The uv is now: " + uvRating) ;
+                String line = null;
+                while ((line = reader.readLine()) != null)
+                {
+                    sb.append(line + "\n");
+                }
+                String result = sb.toString(); //result is the whole string
+
+
+                // convert string to JSON:
+                JSONObject uvReport = new JSONObject(result);
+
+                //get the double associated with "value"
+                Thread.sleep(200);
+                publishProgress(100);
+                double uvRating = uvReport.getDouble("value");
+                uv = Double.toString(uvRating);
+
 
             }
             catch (Exception e) {
@@ -145,6 +159,7 @@ public class WeatherForecast extends AppCompatActivity {
         //Type 2
         public void onProgressUpdate(Integer ... args) {
             progressBar.setVisibility(View.VISIBLE);
+            progressBar.setProgress(args[0]);
 
         }
         //Type3
@@ -153,10 +168,12 @@ public class WeatherForecast extends AppCompatActivity {
             currentTempText.setVisibility(View.VISIBLE);
             minTempText.setVisibility(View.VISIBLE);
             maxTempText.setVisibility(View.VISIBLE);
+            uvRatingText.setVisibility(View.VISIBLE);
 
             minTempText.setText(minTemp);
             maxTempText.setText(maxTemp);
             currentTempText.setText(currentTemp);
+            uvRatingText.setText(uv);
 
             progressBar.setVisibility(View.GONE);
         }
